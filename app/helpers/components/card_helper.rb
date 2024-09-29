6ddbd3d754f81ace06ca6f5e27a9bceeab0a4cec
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-09-29 16:17:45 +0530

    customized toast, cards

--- app/helpers/components/card_helper.rb
+++ app/helpers/components/card_helper.rb
@@ -2,4 +2,8 @@ module Components::CardHelper
   def render_card(title: nil, subtitle: nil, body: nil, footer: nil, **options, &block)
     render "components/ui/card", title: title, subtitle: subtitle, footer: footer, body: (block ? capture(&block) : body), block:, options: options
   end
+
+  def render_dashboard_card(title: nil, subtitle: nil, body: nil, footer: nil, **options, &block)
+    render "components/ui/dashboard_card", title: title, subtitle: subtitle, footer: footer, body: (block ? capture(&block) : body), block:, options: options
+  end
 end
--- app/helpers/components/toast_helper.rb
+++ app/helpers/components/toast_helper.rb
@@ -2,7 +2,7 @@ module Components::ToastHelper
   def render_toast(header: nil, description: nil, action: nil, class: nil, data: {}, variant: :default, **options, &block)
     options[:class] ||= ""
     options[:class] << " destructive group border-destructive bg-destructive text-destructive-foreground " if variant == :destructive
-
+    options[:position_class] = options[:position] == :top ? "right-0 sm:top-0" : "sm:bottom-0 sm:top-auto"
     render "components/ui/toast", header:, description:, action:, class:, data:, options: options
   end
 end
